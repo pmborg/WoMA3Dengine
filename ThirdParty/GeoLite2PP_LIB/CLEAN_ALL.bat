9115b732ea98854e32fe60b1cb648179537f9a2c
pmborg <pmborg@yahoo.com>
2023-03-12 18:02:33 +0000

    Add GeoLite2PP_LIB

---
+++ ThirdParty/GeoLite2PP_LIB/CLEAN_ALL.bat
@@ -0,0 +1,15 @@
+@echo off
+del /S /F /Q .vs 2> nul
+rmdir /S /Q .vs 2> nul
+
+del /S /F /Q Release 2> nul
+rmdir /S /Q Release 2> nul
+
+del /S /F /Q dbgRel 2> nul
+rmdir /S /Q dbgRel 2> nul
+
+del /S /F /Q Debug 2> nul
+rmdir /S /Q Debug 2> nul
+
+del /S /F /Q x64 2> nul
+rmdir /S /Q x64 2> nul
