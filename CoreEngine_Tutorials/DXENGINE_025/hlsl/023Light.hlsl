7f63c5c93e41dbb53557db5886a0903a79d720a6
pmborg <pmborg@yahoo.com>
2023-03-26 11:23:46 +0100

    fix light on 25

--- CoreEngine_Tutorials/DXENGINE_025/hlsl/023Light.hlsl
+++ CoreEngine_Tutorials/DXENGINE_025/hlsl/023Light.hlsl
@@ -230,8 +230,7 @@ SamplerState SampleType: register(s0);
 float4 PSlightFunc2(float3 Normal)
 ////////////////////////////////////////////////////////////////////////////////
 {
-	//return saturate(dot(Normal, -lightDirection));							// Calculate the amount of light on this pixel
-	return saturate(dot(Normal, lightDirection));							// Calculate the amount of light on this pixel
+	return saturate(dot(Normal, -lightDirection));							// Calculate the amount of light on this pixel
 }
 
 ////////////////////////////////////////////////////////////////////////////////
