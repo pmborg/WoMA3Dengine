08f98a823c8bdf8cca56d63c52540f68bdf7fdbe
Pedro Borges <pmborg@yahoo.com>
2025-03-29 18:24:13 +0000

    update clean_ALL_BIN_tutorials.bat

--- CoreEngine_Tutorials/clean_ALL_BIN_tutorials.bat
+++ CoreEngine_Tutorials/clean_ALL_BIN_tutorials.bat
@@ -1,12 +1,6 @@
+ECHO ON
 del /S /F /Q ENGINE_*.txt 2> nul
 del /S /F /Q DXENGINE_*.txt 2> nul
-
-cd ../ThirdParty/DirectXTK
-del /S /F /Q x64 2> nul
-del /S /F /Q Bin 2> nul
-rmdir /S /Q x64 2> nul
-rmdir /S /Q Bin 2> nul
-cd ..
 
 for /D %%f in (CoreEngine_*) do (
 cd %%f
@@ -16,6 +10,15 @@ cd ..
 
 for /D %%f in (DXENGINE_*) do (
 cd %%f
+for /D %%d in (DXENGINE_*) do (
+	rmdir /S /Q %%d 2> nul
+)
 call CLEAN_ALL.bat
 cd ..
 )
+
+rem cd ../ThirdParty/DirectXTK
+rem del /S /F /Q x64 2> nul
+rem del /S /F /Q Bin 2> nul
+rem rmdir /S /Q x64 2> nul
+rem rmdir /S /Q Bin 2> nul
