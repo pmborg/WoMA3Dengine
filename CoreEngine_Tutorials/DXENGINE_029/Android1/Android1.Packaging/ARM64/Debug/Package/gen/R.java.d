16bb2e5fe46e95025863047a8b6f88e024263985
pmborg <pmborg@yahoo.com>
2025-11-03 00:50:52 +0000

    Starting review/settings for Android ARM64, X64 was already ok.

---
+++ CoreEngine_Tutorials/DXENGINE_029/Android1/Android1.Packaging/ARM64/Debug/Package/gen/R.java.d
@@ -0,0 +1,3 @@
+C:\WoMA3Dengine\CoreEngine_Tutorials\DXENGINE_029\Android1\Android1.Packaging\ARM64\Debug\Package\gen\com\woma\R.java \
+ : C:\WoMA3Dengine\CoreEngine_Tutorials\DXENGINE_029\Android1\Android1.Packaging\ARM64\Debug\Package\res\values\strings.xml \
+C:\WoMA3Dengine\CoreEngine_Tutorials\DXENGINE_029\Android1\Android1.Packaging\ARM64\Debug\Package\bin\AndroidManifest.xml \
--- CoreEngine_Tutorials/DXENGINE_029/Android1/Android1.Packaging/src/com/woma/MyActivity.java
+++ CoreEngine_Tutorials/DXENGINE_029/Android1/Android1.Packaging/src/com/woma/MyActivity.java
@@ -7,7 +7,7 @@
 //
 // This file is part of the WorldOfMiddleAge project.
 //
-// The WorldOfMiddleAge project files can not be copied or distributed for comercial use 
+// The WorldOfMiddleAge project files can not be copied or distributed for commercial use 
 // without the express written permission of Pedro Miguel Borges [pmborg@yahoo.com]
 // You may not alter or remove any copyright or other notice from copies of the content.
 // The content contained in this file is provided only for educational and informational purposes.
@@ -16,7 +16,7 @@
 // --------------------------------------------------------------------------------------------
 // PURPOSE: 
 // --------------------------------------------------------------------------------------------
-//WomaIntegrityCheck = 1234567222;
+//WomaIntegrityCheck = 1234525256;
 
 
 // https://www.tutorialspoint.com/android/android_hello_world_example.htm
